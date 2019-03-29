18496715916350727e5d28cbc78e7f41555f56b5
Harper <charlespack2019@u.northwestern.edu>
2019-03-28 20:39:39 -0500

    Pushing earlier code version

---
+++ OLD_GIT/directions.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 01:22:43 2019
+
+@author: harper
+"""
+
+import spacy
+from spacy.lang.en import English
+import en_core_web_sm
+
+from bs4 import BeautifulSoup
+import requests
+
+from ingredients import ingred, convert_to_float, new_ingredient
+
+nlp = en_core_web_sm.load()
+
+class Direction:
+    def __init__(self):
+        self.text = ''
+        self.device = ''
+        self.actions = []
+        self.duration = ()
+        self.time_unit = ''
+        self.ingredients = []
+
+def load_directions(soup):
+#    resp = requests.get(url_string)
+#    soup = BeautifulSoup(resp.text, "lxml")
+    results = soup.find_all('span', class_='recipe-directions__list--item')
+    r_list = []
+    for i in results:
+        if len(i.contents) > 0:
+            for f in range(len(i.contents)):
+                if i.contents[f]:
+                    steps = i.contents[f].split(". ")
+                    for step in steps:
+                        if "." in step:
+                            step = step.rstrip()
+                        else:
+                            step += "."
+                        r_list.append(step)
+    return r_list
+
+# NEED TO MAKE USE OF INGREDIENT LIST TO BETTER CAPTURE INGREDIENTS IN DIRECTIONS
+
+def make_direction(step, names):
+    numerals = [' one ', ' two ', ' three ', ' four ', ' five ', ' six ', ' seven ',
+                ' eight ', ' nine ', ' ten ', ' eleven ', ' twelve ', ' dozen ']
+    numeral_fix = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6,
+                   'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'eleven': 11,
+                   'twelve': 12, 'dozen': 12}
+    cook_verbs = ['preheat', 'cook', 'broil', 'roast', 'drain', 'bake',
+                  'rinse', 'melt', 'stir', 'mix', 'bake', 'simmer', 'season',
+                  'sauté', 'poach', 'whisk', 'stew', 'grill']
+    measure_words = ['cup','teaspoon','tablespoon','ounce','pound','clove', 'stalk', 'pinch']
+    time_words = ['second', 'minute', 'hour', 'seconds', 'minutes', 'hours']
+    cook_nouns = ['oven', 'saucepan', 'pot', 'dish', 'cooker', 'bowl', 'pan',
+                  'stove', 'cups']
+    cn_qualifiers = ['baking', 'mixing', 'slow']
+    agglomerations = ['mixture', 'sauce', 'contents', 'marinade', 'mix', 'bowl']
+    direction = Direction()
+    direction.text = step
+    if any(numeral in direction.text for numeral in numerals):
+        for numeral in numerals:
+            if numeral in direction.text:
+                find = numeral.strip()
+                replace = numeral_fix[find]
+                direction.text = direction.text.replace(numeral, ' ' + str(replace) + ' ')
+    doc = nlp(direction.text)
+    # tokenize each instructions step
+    tokens = list(doc)
+    for token in tokens:
+        j = tokens.index(token)
+        if tokens[j].is_punct:
+            continue
+        # if the token is a cooking action, store it as such
+        if tokens[j].text.lower() in cook_verbs:
+            direction.actions.append(tokens[j].text)
+        elif tokens[j].tag_ == 'VB' and tokens[j].lemma_ == 'heat':
+            direction.actions.append(tokens[j].text)
+        elif tokens[j].lemma_ == 'boil' or tokens[j].lemma_ == 'boiling':
+            direction.actions.append(tokens[j].text)
+        # numbers may pertain to time or quantities, both of which we want
+        elif j < len(tokens) and tokens[j].tag_ == 'CD' or tokens[j].tag_ == 'LS':
+            # often we see "X to Y time_units"
+            if tokens[j+1].text == 'to':
+                direction.time_unit = tokens[j+3].lemma_
+                direction.duration = (int(tokens[j].text),int(tokens[j+2].text))
+#                dur = tokens[j].text + ' to ' + tokens[j+2].text
+#                direction.duration = dur
+                del tokens[j:j+4]
+            # may also see "X time_units"
+            elif tokens[j+1].lemma_ in time_words:
+                direction.time_unit = tokens[j+1].lemma_
+                direction.duration = (int(tokens[j].text), int(tokens[j].text))
+                del tokens[j:j+2]
+            # may also refer to ingredient quantities - e.g., 'X units of...'
+            elif tokens[j+1].lemma_ in measure_words:
+                # if j is followed by measure, it's likely a quantity
+                quantity = convert_to_float(tokens[j].text)
+                unit = tokens[j+1].lemma_
+                # check if form of 'X units ingredient'
+                if tokens[j+2].tag_ == 'NN' or tokens[j+2].tag_ == 'NNP':
+                    # check if ingredient has multi-part name (e.g., Parm cheese)
+                    if j+3 < len(tokens) and tokens[j+3].tag_ == 'NN':
+                        name = tokens[j+2].text + ' ' + tokens[j+3].text
+                        item = new_ingredient(name, quantity, unit)
+                        del tokens[j:j+4]
+                    else:
+                        name = tokens[j+2].text
+                        item = new_ingredient(name, quantity, unit)
+                        del tokens[j:j+3]
+                # check if form of 'X units OF ingredient'
+                elif tokens[j+2].tag_ == 'IN' and tokens[j+3].tag_ == 'NN' or tokens[j+3].tag_ == 'NNP':
+                    # check if ingredient has multi-part name (e.g., Parm cheese)
+                    if j+4 < len(tokens) and tokens[j+4].tag_ == 'NN':
+                        name = tokens[j+3].text + ' ' + tokens[j+4].text
+                        item = new_ingredient(name, quantity, unit)
+                        del tokens[j:j+5]
+                    else:
+                        name = tokens[j+3].text
+                        item = new_ingredient(name, quantity, unit)
+                        del tokens[j:j+4]
+                    direction.ingredients.append(item)
+            elif tokens[j+1].text == 'of':
+                jth = 0
+                quantity = convert_to_float(tokens[j].text)
+                if tokens[j+2].text == 'the':
+                    if tokens[j+3].lemma_ in agglomerations:
+                        if tokens[j+4].lemma_ in agglomerations:
+                            name = tokens[j+3].text + ' ' + tokens[j+4].text
+                            jth = 5
+                        else:
+                            name = tokens[j+3].text
+                            jth = 4
+                        item = new_ingredient(name, quantity, 'ratio')
+                        item.type = 'combination'
+                        del tokens[j:j+jth]
+                    elif tokens[j+4].lemma_ in agglomerations:
+                        if tokens[j+5].lemma_ in agglomerations:
+                            name = tokens[j+4].text + ' ' + tokens[j+5].text
+                            jth = 6
+                        else:
+                            name = tokens[j+4].text
+                            jth = 5
+                        item = new_ingredient(name, quantity, 'ratio')
+                        item.type = 'combination'
+                        del tokens[j:j+jth]
+                    elif any(name in tokens[j+3].lemma_ for name in names):
+                        if any(name in tokens[j+4].lemma_ for name in names):
+                            name = tokens[j+3].text + ' ' + tokens[j+4].text
+                            jth = 5
+                        else:
+                            name = tokens[j+3].text
+                            jth = 4
+                        item = new_ingredient(name, quantity, 'ratio')
+                        del tokens[j:j+jth]
+            elif tokens[j+1].tag_ == 'CD' or tokens[j+1].tag_ == 'LS':
+                combined = convert_to_float(tokens[j].text)
+                combined += convert_to_float(tokens[j+1].text)
+                if tokens[j+2].lemma_ in time_words:
+                    direction.duration = (int(combined), int(combined))
+                    direction.time_unit = tokens[j+2].text
+                    del tokens[j:j+3]
+        # check if token is a cooking implement
+        elif tokens[j].text in cook_nouns:
+            # check if it has a multi-part name (e.g., 'slow cooker')
+            if j > 0 and tokens[j-1].text in cn_qualifiers:
+                name = tokens[j-1].text + ' ' + tokens[j].text
+                direction.device = name
+            else:
+                direction.device = tokens[j].text
+        elif tokens[j].text == 'until':
+            if not direction.duration:
+                check = [x.text for x in tokens[j+1:]]
+                if not any(word in time_words for word in check):
+                    until_dur = tokens[j].text
+                    for word in tokens[j+1:]:
+                        if word.is_punct:
+                            break
+                        until_dur += ' ' + word.text
+                    direction.duration = until_dur
+                    direction.time_unit = 'subjective'
+    return direction
+
+#load_directions('https://www.allrecipes.com/recipe/8758/white-cheese-chicken-lasagna/')
---
+++ OLD_GIT/ingredients.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 00:48:41 2019
+
+@author: harper
+"""
+#url_test = 'https://www.allrecipes.com/recipe/257306/easy-skinless-fried-chicken-thighs/'
+#url_test = 'https://www.allrecipes.com/recipe/162894/black-pepper-goat-curry/'
+#url_test = 'https://www.allrecipes.com/recipe/234621/rusty-chicken-thighs/
+#url_test = 'https://www.allrecipes.com/recipe/140897/mimis-smoked-salmon-chowder/'
+
+from bs4 import BeautifulSoup
+import requests
+import spacy
+
+#url_test = 'https://www.allrecipes.com/recipe/8130/sour-cream-coffee-cake-iii/'
+#url_test = 'https://www.allrecipes.com/recipe/8149/flourless-chocolate-cake-i/'
+
+url_test = 'https://www.allrecipes.com/recipe/234621/rusty-chicken-thighs/'
+
+class ingred:
+    def __init__(self):
+        self.quantity = 0
+        self.name = ''
+        self.unit = ''
+        self.preprocessing = []
+        self.descriptors = []
+        self.alternative = ''
+        self.type = ''
+        self.specified = ''
+        self.flags = []
+        self.method = []
+        self.old = None
+        self.new = None
+    
+    def more(self, new):
+        if self.unit == new.unit:
+            self.quantity += new.quantity
+        else:
+            if self.unit == 'pound':
+                self.quantity += new.quantity / 16
+            elif self.unit == 'ounce':
+                self.quantity += new.quantity * 16
+
+def new_ingredient(name, q=None, u=None, p=None, d=None):
+    new = ingred()
+    new.name = name
+    if q:
+        new.quantity = q
+    if u:
+        new.unit = u
+    if p:
+        new.preprocessing = p
+    if d:
+        new.descriptors = d
+    return new
+
+def convert_to_float(frac_str):
+    try:
+        return float(frac_str)
+    except ValueError:
+        try:
+            num, denom = frac_str.split('/')
+        except ValueError:
+            return None
+        try:
+            leading, num = num.split(' ')
+        except ValueError:
+            return float(num) / float(denom)        
+        if float(leading) < 0:
+            sign_mult = -1
+        else:
+            sign_mult = 1
+        return float(leading) + sign_mult * (float(num) / float(denom))
+
+def rationalize_details(new, servings):
+    if 'mushroom' in new.name:
+        if 'portobello' in new.name:
+            new.quantity = float(1.5 * int(servings))
+            new.unit = 'discrete'
+            new.preprocessing = []
+            new.descriptors = []
+        elif 'shiitake' in new.name:
+            new.quantity = float(0.5 * int(servings))
+            new.unit = 'cup'
+            new.preprocessing = ['sliced']
+            new.descriptors = ['fresh']
+    elif 'jackfruit' in new.name:
+        new.quantity = float(int(servings)/3)
+        new.unit = 'pound'
+        new.preprocessing = ['drained']
+        new.descriptors = ['young', 'green', 'packed in water']
+    elif 'eggplant' in new.name:
+        new.quantity = float(0.5 * int(servings))
+        new.unit = 'discrete'
+        # ARE THESE RIGHT?
+#        new.preprocessing = []
+#        new.descriptors = []
+    elif 'tofu' in new.name:
+        new.quantity = float(0.5 * int(servings))
+        new.unit = 'block'
+        # ARE THESE RIGHT?
+        new.preprocessing.append('drained')
+        new.descriptors.append('extra-firm')
+    elif 'olive oil' in new.name:
+        if 'oil' not in new.old.name:
+            if new.old.unit in ['cup','teaspoon','tablespoon', 'tsp', 'tbsp']:
+                new.quantity = 0.75 * new.old.quantity
+            elif new.old.unit == 'stick':
+                q = 8 * new.old.quantity
+                new.quantity = q * 0.75
+                new.unit = 'tablespoon'
+            elif new.old.unit == 'inch':
+                q = 1.6 * new.old.quantity
+                new.quantity = q * 0.75
+                new.unit = 'tablespoon'
+    elif 'avocado' in new.name:
+        new.quantity = float(0.5 * int(servings))
+        new.unit = 'discrete'
+        new.preprocessing = ['peeled', 'pitted', 'diced']
+        new.descriptors = ['fresh']
+    return new
+
+def is_bad(tok):
+    if tok.pos_ in ['ADJ','PUNCT'] or tok.tag_ in ['CC','VBN']:
+        return True
+    for i in tok.ancestors:
+        if is_bad(i):
+            return True
+    return False
+
+def load_ingredients(soup):
+#    resp = requests.get(url_string)
+#    soup = BeautifulSoup(resp.text, "lxml")
+    results = soup.find_all('span', class_='recipe-ingred_txt')
+    r_list = []
+    for i in results:
+        if len(i.contents) > 0:
+            if i.contents[0] != 'Add all ingredients to list':
+                r_list.append(str(i.contents[0]))
+    return r_list
+
+def make_ingredient(i):
+    measure_words = ['cup','teaspoon', 'tsp','tablespoon', 'tbsp','tbl''ounce','pound','clove', 'stalk','inch','cm','mm','centimeter','millimeter','square','can','bottle', 'glass', 'box', 'quart',
+                    'quarter','kernels', 'ears', 'bushels', 'drop', 'pail', 'pinch', 'celsius', 'fahrenheit', 'degrees' 'militer', 'pint', 'pt', 'litre','gram', 'gill',
+                    'gallon', 'ml','l','liter','mg', 'milligram','g','gram','kg', 'kilogram', 'meter', 'oz','lb', 'fluid ounce', 'dash']
+    adj_exclude = ['sour','garlic', 'spicy', 'sweet', 'bitter', 'crunchy','whip','sauteed', 'sautéed', 'glaze' ]
+    avoid_list = []
+    nlp = spacy.load('en_core_web_sm')
+    info_struct = ingred()
+    doc = nlp(i)
+    tokens = list(doc)
+    if len(tokens) < 2:
+        return None
+    if tokens[0].text.lower() == 'a':
+        del tokens[0:1]
+    if tokens[0].tag_ == 'CD' or tokens[0].tag_ == 'LS':
+        if tokens[1].tag_ == 'CD' or tokens[1].tag_ == 'LS':
+            q1 = convert_to_float(str(tokens[0]))
+            q2 = convert_to_float(str(tokens[1]))
+            info_struct.quantity = q1 + q2
+            del tokens[0:2]
+        else:
+            info_struct.quantity = convert_to_float(str(tokens[0]))
+            del tokens[0:1]
+        if tokens[0].lemma_ in measure_words:
+            info_struct.unit = tokens[0].lemma_
+            del tokens[0:1]
+        else:
+            info_struct.unit = 'discrete'
+        if tokens[0].text == '(':
+            end_paren_ix = 1
+            while tokens[end_paren_ix].text != ')':
+                end_paren_ix += 1
+            info_struct.unit = "".join(j.text+' ' for j in tokens[0:end_paren_ix+1] if j.pos_ != 'PUNCT').strip()
+            if tokens[end_paren_ix+1].text in measure_words:
+                info_struct.unit = info_struct.unit.join(' ' + tokens[end_paren_ix+1])
+                del tokens[0:end_paren_ix+2]
+            else:
+                del tokens[0:end_paren_ix+1]
+            
+    #deal with the 'or' case
+    for j in range(0,len(tokens)-1):
+        if tokens[j].text == 'or':
+            info_struct.alternative = ''.join(k.text+' ' for k in tokens[j+1:]).strip()
+            del tokens[j:]
+            break
+    newdoc = nlp("".join(j.text_with_ws for j in tokens))
+    chunk_list = list(newdoc.noun_chunks)
+    if len(chunk_list) > 0:
+        for j in chunk_list[0].root.children:
+            if j.tag_ == 'VBN':
+                info_struct.preprocessing.append(''.join(w.text_with_ws for w in j.subtree if w.pos_ not in ['PUNCT'] and w != chunk_list[0].root))
+                for k in j.subtree:
+                    if k != chunk_list[0].root:
+                        avoid_list.append(k.text)
+            elif j.pos_ == 'ADJ':
+                if j.text not in adj_exclude:
+                    info_struct.descriptors.append(''.join(w.text_with_ws for w in j.subtree if w.pos_ not in ['PUNCT']))
+                    for k in j.subtree:
+                        avoid_list.append(k.text)
+        info_struct.name = ''.join(w.text + ' ' for w in chunk_list[0].root.subtree if w.text not in avoid_list and w.pos_ != 'PUNCT').strip()
+    else:
+        for j in newdoc:
+            if j.tag_ == 'VBN':
+                info_struct.preprocessing.append(''.join(w.text_with_ws for w in j.subtree if w.pos_ not in ['NOUN','PUNCT']))
+                for k in j.subtree:
+                    if k.pos_!='NOUN':
+                        avoid_list.append(k.text)
+        info_struct.name = ''.join(w.text + ' ' for w in newdoc if w.text not in avoid_list and w.pos_ != 'PUNCT')
+#    print('Name:' + info_struct.name)
+#    print('Quantity:' + str(info_struct.quantity))
+#    print('Unit: ' + info_struct.unit)
+#    print('Descriptors: ' + str(info_struct.descriptors))
+#    print('Preprocessing: ' + str(info_struct.preprocessing))
+    return info_struct
+
+#tests = load_ingredients(url_test)
+#ingredients = []
+#for test in tests:
+#    result = make_ingredient(test)
+#    if result:
+#        ingredients.append(result)
+#        print(result.name)
